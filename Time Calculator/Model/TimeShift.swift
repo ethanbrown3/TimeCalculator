afcf45856a4161741086af7ac782b7bb68378d46
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-03-10 00:07:33 -0700

    Created TimeShift Model

---
+++ Time Calculator/Model/TimeShift.swift
@@ -0,0 +1,16 @@
+//
+//  TimeShift.swift
+//  Time Calculator
+//
+//  Created by Ethan Brown on 3/9/18.
+//  Copyright © 2018 Ethan Brown. All rights reserved.
+//
+import UIKit
+
+class TimeShift {
+    
+    var startTime : String = ""
+    var endTime : String = ""
+    var totalTime : String = ""
+    
+}
